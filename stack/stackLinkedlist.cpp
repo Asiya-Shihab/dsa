7ebc9afcf32cacf53086240d6a39c7b4107610ff
Asiya-Shihab <143543296+Asiya-Shihab@users.noreply.github.com>
2025-06-09 14:35:40 +0530

    Create stackLinkedlist.cpp

---
+++ stack/stackLinkedlist.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node * next;
+}*top=NULL;
+void push(int x){
+    Node * t=new Node;
+    if(t==NULL)
+    cout<<"stack overflow ";
+    else 
+    t->data=x;
+    t->next=top;
+    top=t;
+}
+int pop(){
+    Node * p;
+    int x=-1;
+    if(top==NULL){
+        cout<<"stack is empty ";
+
+    }
+    else{
+        p=top;
+        
+        top=top->next;
+        x=p->data;
+        delete []p;
+        p=NULL;
+        
+        
+    }
+    return x;
+}
+int peek(int pos){
+    Node * p=top;
+    for(int i=0;p&&i<pos-1;i++){
+        p=p->next;
+    }
+    if(p)
+    return p->data;
+    else 
+    return -1;
+}
+void display(){
+    Node * p=top;
+    while(p){
+        cout<<p->data<<" ";
+        p=p->next;
+
+    }
+    cout<<endl;
+}
+int main(){
+    push(3);
+    push(2);
+    push(1);
+    push(4);
+    push(5);
+    display();
+   cout<<"element at postion 2 is "<<peek(2)<<endl;
+    pop();
+    pop();
+    pop();
+    display();
+
+}
